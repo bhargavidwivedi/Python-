86193c30817bfe501551e8efc07179672615af33
bhargavidwivedi <bhargavidwivedi56@gmail.com>
2025-11-12 19:07:02 +0530

    Smallest Number

---
+++ Write a program to find the smaller number among the two numbers.py
@@ -0,0 +1,10 @@
+
+def smallest_number(a,b):
+    if a > b:
+        print("b is the smallest")
+    else:
+        print("a is the smallest")
+
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second  number:"))
+smallest_number(num1, num2)
