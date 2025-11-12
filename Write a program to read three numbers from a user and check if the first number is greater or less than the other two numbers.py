1518296fdd2f89e87105cbd4ff4a87790e2c72d9
bhargavidwivedi <bhargavidwivedi56@gmail.com>
2025-11-12 22:33:52 +0530

    Greatest

---
+++ Write a program to read three numbers from a user and check if the first number is greater or less than the other two numbers.py
@@ -0,0 +1,12 @@
+
+def greater(a,b,c):
+    if a > b and a > c:
+         print("a is greatest")
+    else:
+         print("a is not greatest")
+
+a = float(input("Enter the number:"))
+b = float(input("Enter the number:"))
+c = float(input("Enter the number:"))
+
+greater(a,b,c)
